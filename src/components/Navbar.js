937f54fc80eb6623c4b48ebe9df354caa0491371
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-08-17 20:17:00 +0500

    for testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ function Navbar() {
           <ul className="links">
             <li><a href="#home" className="link">HOME</a></li>
             <li><a href="#about" className="link">ABOUT</a></li>
-            <li><a href="#contact" className="link">CONTACT</a></li>
-            <li><a href="#portfolio" className="link">PORTFOLIO</a></li>
+            {/* <li><a href="#contact" className="link">CONTACT</a></li>
+            <li><a href="#portfolio" className="link">PORTFOLIO</a></li> */}
           </ul>
       </div>
     </>
