24b3346fa4797b3b90b16f08409f2640590d220d
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-08-22 18:05:24 +0500

    Portfolio animated

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PortfolioItem from "./PortfolioItem";
 import { PROJECTINFORMATION } from "./ProjectInformation";
-import "../stylesheets/Portfolio.css"
+import "../stylesheets/Portfolio.css";
 
 function Portfolio() {
+  
   const projectInformation = PROJECTINFORMATION;
 
-  const portfolioItems = projectInformation.map(item => <PortfolioItem
-    img={item.image}
-    title={item.title}
-    description={item.description}
-    siteLink={item.siteLink}
-    codeLink={item.codeLink}
-  />)
+  const portfolioItems = projectInformation.map((item) => (
+    <PortfolioItem
+      img={item.image}
+      title={item.title}
+      description={item.description}
+      siteLink={item.siteLink}
+      codeLink={item.codeLink}
+    />
+  ));
   return (
     <>
       <div id="portfolio" className="portfolio">
         <h1 className="portfolio-title">Projects I've made</h1>
-        <div className="portfolio-items">
-          {portfolioItems}
-        </div>
+        <div className="portfolio-items">{portfolioItems}</div>
       </div>
     </>
   );
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../stylesheets/PortfolioItem.css";
 
 function PortfolioItem(props) {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const options = {
+    root: null,
+    rootMargin: "100px 0px 100px 0px",
+    threshold: 0.85,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      setIsVisible(entry.isIntersecting);
+    }, options);
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    };
+  }, [containerRef, options]);
+
   return (
-    <div className="portfolio-item">
-      <div className="img-container container">
+    <div ref={containerRef} className="portfolio-item">
+      <div 
+        className={
+          isVisible
+            ? "container-animation container img-container"
+            : "container img-container"
+        }
+      >
         <img src={props.img} alt="" className="project-img" />
       </div>
-      <div className="text-container container">
+      <div className={
+          isVisible
+            ? "container-animation container text-container"
+            : "container text-container"
+        }>
         <h1 className="project-title">{props.title}</h1>
         <p className="project-description">{props.description}</p>
         <div className="site-links">
-          <a href={props.siteLink} className="view-site" target="_blank" rel="noreferrer">
+          <a
+            href={props.siteLink}
+            className="view-site"
+            target="_blank"
+            rel="noreferrer"
+          >
             View Site
           </a>
           <a href={props.codeLink} className="view-code" target="_blank">
