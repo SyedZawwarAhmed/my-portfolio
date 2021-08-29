8329a6547d745f0e8949b3409b8d2bfc15d9526c
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-08-29 21:59:43 +0500

    hamburger menu added

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/Hamburger.css";
 
 function Hamburger() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
     <>
-      <div className="hamburger"></div>
-      <div className="menu">
-        <ul className="hamburger-links">
-          <li>
-            <a href="#home" className="hamburger-link">
-              HOME
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" className="hamburger-link">
-              PORTFOLIO
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="hamburger-link">
-              ABOUT
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="hamburger-link">
-              CONTACT
-            </a>
-          </li>
-        </ul>
+      <div className={isMenuOpen ? "hamburger-menu open" : "hamburger-menu"}>
+        <div className="mask" onClick={toggleMenu}></div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="bar"></div>
+        </div>
+        <div className="menu">
+          <ul className="hamburger-links">
+            <li>
+              <a href="#home" className="hamburger-link" onClick={toggleMenu}>
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="hamburger-link" onClick={toggleMenu}>
+                PORTFOLIO
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="hamburger-link" onClick={toggleMenu}>
+                ABOUT
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="hamburger-link" onClick={toggleMenu}>
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
