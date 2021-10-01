0b60a91984469346857547a675f1c44d063d512d
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-10-01 14:14:18 +0500

    new project added

--- src/components/ProjectInformation.js
+++ src/components/ProjectInformation.js
@@ -1,4 +1,5 @@
 import coffee from "../assets/images/screenshots/Coffee.png";
+import jobListings from "../assets/images/screenshots/JobListings.png";
 import bank from "../assets/images/screenshots/Easybank.png";
 import pricing from "../assets/images/screenshots/Pricing.png";
 import countries from "../assets/images/screenshots/Countries.png";
@@ -12,34 +13,45 @@ export const PROJECTINFORMATION = [
       "This is a multipage coffee subscription site made using HTML, CSS and Vanilla JavaScript.",
     siteLink:
       "https://syedzawwarahmed.github.io/coffeeroasters-subscription-site/",
-      codeLink: 'https://github.com/SyedZawwarAhmed/coffeeroasters-subscription-site'
+    codeLink:
+      "https://github.com/SyedZawwarAhmed/coffeeroasters-subscription-site",
   },
   {
-    id:1,
+    id: 1,
     image: countries,
     title: "Rest Countries API",
     description:
       "This application displays information about all the countries of the world by integrating with a REST API.",
     siteLink: "https://syedzawwarahmed.github.io/Rest-Countries-API/",
-    codeLink: 'https://github.com/SyedZawwarAhmed/Rest-Countries-API'
+    codeLink: "https://github.com/SyedZawwarAhmed/Rest-Countries-API",
   },
   {
     id: 2,
+    image: jobListings,
+    title: "Static Job Listings",
+    description:
+      "This is a Reactjs site which shows programming jobs from a data file and those jobs can be filtered based on the role, level and programming languages.",
+    siteLink: "https://staticjoblistings.netlify.app/",
+    codeLink: "https://github.com/SyedZawwarAhmed/static-job-listings",
+  },
+  {
+    id: 3,
     image: bank,
     title: "Easybank Landing Page",
     description:
       "This is a landing page website for a bank made using HTML, CSS and a little bit of JavaScript.",
     siteLink: "https://syedzawwarahmed.github.io/easybank-landing-page/",
-    codeLink: 'https://github.com/SyedZawwarAhmed/easybank-landing-page'
+    codeLink: "https://github.com/SyedZawwarAhmed/easybank-landing-page",
   },
   {
-    id: 3,
+    id: 4,
     image: pricing,
     title: "Interactive Pricing Component",
     description:
       "This is a simple project which gives a pricing plan based on user preferences.",
     siteLink:
       "https://syedzawwarahmed.github.io/interactive-pricing-component/",
-      codeLink: 'https://github.com/SyedZawwarAhmed/interactive-pricing-component'
+    codeLink:
+      "https://github.com/SyedZawwarAhmed/interactive-pricing-component",
   },
 ];
